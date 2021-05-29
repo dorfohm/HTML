c6d948b2c54838e6bb244e1bafd4f51a9994fe94
dorfohm <dorfohm@hotmail.com>
2021-05-29 14:10:36 -0300

    Finalmente consegui fazer o git voltar a enxergar as mudanças nessa merda de pasta, obrigado git rm --cached

---
+++ meat-app-starter/out-tsc/app/app/app.routes.js
@@ -0,0 +1,9 @@
+import { AboutComponent } from "./about/about.component";
+import { HomeComponent } from "./home/home.component";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
+export var ROUTES = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'restaurants', component: RestaurantsComponent }
+];
+//# sourceMappingURL=app.routes.js.map
